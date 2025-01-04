4ed21b978e8af7086a6b3226a10b46aa482dfd57
egorvshch <42111678+egorvshch@users.noreply.github.com>
2025-01-04 21:56:46 +0500

    Delete otus_projekt_work directory

--- otus_projekt_work/ansible/dbslave/files/backup_db.sh
+++
@@ -1,6 +0,0 @@
-# Creating new dump of -wpress_db- database with necessary tables
-mysqldump --all-databases --add-drop-table --create-options --disable-keys \
-                   --extended-insert --single-transaction --quick --events --routines \
-                   --set-gtid-purged=OFF --triggers --source-data=0 --skip-lock-tables \
-                   --flush-privileges \
-                   -u root -ppass123 > /home/vagrant/db_backup/db.sql
